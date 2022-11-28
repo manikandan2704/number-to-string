e005c4a764a84ac5e16a17f95653a7a99a7bebd8
manikandan <manimani19508@gmail.com>
2022-11-28 12:53:16 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -65,20 +65,33 @@
 
    
 
-var num=12321;
-var sum=0;
-var c=num;
+// var num=12321;
+// var sum=0;
+// var c=num;
 
-while (num<0){
-    b=num%10;
-    sum=sum*10+b;
-    num=num+(num/10);
-}
-console.log(sum)
+// while (num<0){
+//     b=num%10;
+//     sum=sum*10+b;
+//     num=num+(num/10);
+// }
+// console.log(sum)
 
+// const ascent=()=>{
 
-
-
+//     let overall=""
+//     let inp=parseInt(Document.getElementByid(inp).value)
+//     let inp1=parseInt(Document.getElementByid(inp1).value)
+//     let inp2=parseInt(Document.getElementByid(inp2).value)
+//     let ascent=document.getElementById("display")
+    
+    
+//     overall=parseInt(inp+inp1+inp2)
+//     overall.sort((p1,p2)=>{
+//         return(
+//             p1-p2
+//         )
+//     })
+    // }
 
 
 
